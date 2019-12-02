13e23f68360ac692a5af4777a5c876179389f9bf
Cledson Medeiros <cledson.lmedeiros@hotmail.com>
2019-12-02 10:49:15 -0300

    clean

--- backend/controllers/CardController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\controllers;
-
-class CardController extends app\components\controllers\ActiveController {
-  public $modelClass = 'app\models\Card';
-}
--- backend/controllers/ColumnController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace app\controllers;
-
-class ColumnController extends app\components\controllers\ActiveController {
-  public $modelClass = 'app\models\Column';
-}
